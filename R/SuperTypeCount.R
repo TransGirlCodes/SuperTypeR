ba4753071e1e5121c488700590e9ebff3bff833b
Ward9250 <axolotlfan9250@gmail.com>
2016-02-02 16:45:23 +0000

    mmmm

--- R/SuperTypeCount.R
+++ R/SuperTypeCount.R
@@ -13,8 +13,8 @@ countSuperTypesPerPop <- function(samples){
   colnames(countMatrix) <- as.character(unique(samples$Site))
   rownames(countMatrix) <- as.character(unique(samples$Supertype))
   updateCountMatrix <- function(sampleRow){
-    countMatrix[as.character(sampleRow[1,5]), as.character(sampleRow[1,2])] <<-
-      countMatrix[as.character(sampleRow[1,5]), as.character(sampleRow[1,2])] + 1
+    countMatrix[as.character(sampleRow[5]), as.character(sampleRow[2])] <<-
+      countMatrix[as.character(sampleRow[5]), as.character(sampleRow[2])] + 1
   }
   apply(samples, 1, function(x){updateCountMatrix(x)})
   return(countMatrix)
